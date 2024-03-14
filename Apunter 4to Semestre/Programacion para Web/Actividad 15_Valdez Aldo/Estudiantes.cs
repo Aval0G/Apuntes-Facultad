10a283e40af4a40ca587ecceabe7a87a5bbe419b
Aval0G <avaldez0@ucol.mx>
2024-03-14 08:16:50 -0600

    Programacion Web Act 15

---
+++ Apunter 4to Semestre/Programacion para Web/Actividad 15_Valdez Aldo/Estudiantes.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+public class Estudiante
+{
+    public string Nombre { get; set; } = "";
+    public string Correo { get; set; } = "";
+    public int Calificacion { get; set; }
+
+    public Estudiante(string nombre, string correo, int calificacion)
+    {
+        Nombre = nombre;
+        Correo = correo;
+        Calificacion = calificacion;
+    }
+
+    public bool EsNombreValido()
+    {
+        return !string.IsNullOrWhiteSpace(Nombre);
+    }
+
+    public bool EsCorreoValido()
+    {
+        try
+        {
+            var addr = new System.Net.Mail.MailAddress(Correo);
+            return addr.Address == Correo;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public bool EsCalificacionValida()
+    {
+        return Calificacion >= 0 && Calificacion <= 10;
+    }
+
+    public void ActualizarInformacion(string nombre, string correo, int calificacion)
+    {
+        if (EsNombreValido())
+        {
+            Nombre = nombre;
+        }
+
+        if (EsCorreoValido())
+        {
+            Correo = correo;
+        }
+
+        if (EsCalificacionValida())
+        {
+            Calificacion = calificacion;
+        }
+    }
+}
